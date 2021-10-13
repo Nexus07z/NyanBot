c0c570ea77db4bfdbdd880829af9d44adc7b8693
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2021-10-12 23:30:14 -0500

    Add files via upload

--- samu.js
+++ samu.js
@@ -1704,7 +1704,7 @@ Hola *${pushname}* ${timeFt}
 				reply(mess.wait)
 				nobg = await imgbb('20a14861e4f7591f3dc52649cb07ae02', media);
 				link = `${nobg.display_url}`;
-				get_result = await getJson(`ttps://api.vhtear.com/removebgwithurl?link=${link}&apikey=${apivh}`)
+				get_result = await getJson(`https://api.vhtear.com/removebgwithurl?link=${link}&apikey=${apivh}`)
 				get_result = get_result.result
 				short = await getJson(`https://api.lolhuman.xyz/api/shortlink?apikey=${api}&url=${get_result.image}`)
 		
